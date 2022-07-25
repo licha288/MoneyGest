c962efa4c2740706e719fbc090175d4e395c6ffe
Lisandro Lucca <lisandroluccamoreno@gmail.com>
2022-07-25 20:36:17 -0300

    e-comerce

---
+++ src/Components/navbar.js
@@ -0,0 +1,11 @@
+const NavBar = ()=>{
+    <header>
+        <h1 className="Titulo">Money Gest</h1>
+        <ul className="navbar">
+            <li><button className="boton">inicio</button></li>
+            <li><button className="boton">mis billeteras</button></li>
+            <li><button className="boton">ahorros</button></li>
+        </ul>
+    </header>
+}
+export default NavBar
